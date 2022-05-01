d83c251f63eba17c6547b3a860ff7233542d9fa2
Egor-Poduto <102541066+Egor-Poduto@users.noreply.github.com>
2022-05-01 22:26:09 +0700

    Update Skitala.py
    Добавлена функция дешифровки.

--- Skitala.py
+++ Skitala.py
@@ -5,7 +5,13 @@ def encode (text, lenght, count_string, count_colum):
         encrypted_text[index] = text[i]
     encrypted_text = "".join(encrypted_text)
     print ("Encrypted text: ", encrypted_text)
-
+def decode (text, lenght, count_string, count_colum):
+    decrypted_text = [None] * lenght
+    for i in range (0, lenght):
+        index = int(count_colum * (i % count_string) + (i / count_string))
+        decrypted_text[index] = text[i]
+    decrypted_text = "".join(decrypted_text)
+    print ("Decrypted text: ", decrypted_text)
 print ("Enter text: ")
 text = input ()
 lenght = len(text)
