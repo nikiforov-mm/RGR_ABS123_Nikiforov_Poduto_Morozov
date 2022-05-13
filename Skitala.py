57ff2856d614beba3c4f2368de52e7ea5d238975
Egor-Poduto <102541066+Egor-Poduto@users.noreply.github.com>
2022-05-13 20:20:16 +0700

    Update Skitala.py

--- Skitala.py
+++ Skitala.py
@@ -1,26 +1,20 @@
-def encode (text, lenght, count_string, count_colum):
+def encode (start):
+    lenght = len(start)
     encrypted_text = [None] * lenght
+    print ("Enter key: ")
+    count_string = int(input())
+    count_colum = int(((lenght - 1) / count_string) + 1)    
     for i in range(0, lenght):
         index = int(count_string * (i % count_colum) + (i / count_colum))
-        encrypted_text[index] = text[i]
+        encrypted_text[index] =start[i]
     encrypted_text = "".join(encrypted_text)
-    print ("Encrypted text: ", encrypted_text)
-def decode (text, lenght, count_string, count_colum):
+    return encrypted_text, count_string
+def decode (encrypted_text, count_string):
+    lenght = len(encrypted_text)
+    count_colum = int(((lenght - 1) / count_string) + 1) 
     decrypted_text = [None] * lenght
     for i in range (0, lenght):
         index = int(count_colum * (i % count_string) + (i / count_string))
-        decrypted_text[index] = text[i]
+        decrypted_text[index] = encrypted_text[i]
     decrypted_text = "".join(decrypted_text)
-    print ("Decrypted text: ", decrypted_text)
-print ("Enter text: ")
-text = input ()
-lenght = len(text)
-print ("Enter key: ")
-count_string = int(input())
-count_colum = int(((lenght - 1) / count_string) + 1)
-print ("To encrypt - press 1, to decrypt - press 0")
-button = int(input ())
-if button == int("1"):
-	encode (text, lenght, count_string, count_colum)
-if button == int("0"):
-	decode (text, lenght, count_string, count_colum)
+    return decrypted_text
